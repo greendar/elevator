f1a3d729a0b2815ec7c0811595240dc4eff8cbed
Mr Green <dwgreen@hwdsb.on.ca>
2023-03-04 08:29:34 -0500

    initial coding

--- elevClass.cpp
+++ elevClass.cpp
@@ -1,1 +1,25 @@
 //elevClass.cpp
+#include <iostream>
+using namespace std;
+
+
+class Elevator {
+    int floor;
+    bool doorClosed;
+
+public:
+    Elevator();
+    void returnFloorState();
+
+};
+
+Elevator::Elevator(){
+    floor = 1; // initial floor state is 1
+    doorClosed = true;
+
+
+}
+
+void Elevator::returnFloorState(){
+
+}
